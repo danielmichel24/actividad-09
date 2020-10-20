841b192e1133bbdf9d2c22b787accdf10a30eda4
Daniel Neri <daniel.neri5575@alumnos.udg.mx>
2020-10-20 17:37:50 -0500

    implementamos el recuperar y respaldar en el main para que lo imprima

--- main.cpp
+++ main.cpp
@@ -7,29 +7,35 @@ using namespace std;
 
 int main()
 {
-    Computadora c01 = Computadora("windows","vaio",8,500);
-    Computadora c02;
-    c02.setSo("MAC");
-    c02.setMarca("apple2");
-    c02.setRam(7);
-    c02.setRom(1000);
+    Laboratorio c;
+    c.recuperar();
+    c.recuperar();
+    c.mostrar();
 
-    Laboratorio vj;
+   // Computadora c01 = Computadora("windows","vaio",8,500);
+    //Computadora c02;
+    //c02.setSo("MAC");
+    //c02.setMarca("apple2");
+   // c02.setRam(7);
+    //c02.setRom(1000);
+
+    //Laboratorio vj;
 
     //vj.agregarComputadora(c01);
     //vj.agregarComputadora(c02);
-    vj << c01 << c02;
+    //vj << c01 << c02;
 
-    Computadora c03;
-    cin >> c03;
-    vj.agregarComputadora(c03);
+    //Computadora c03;
+    //cin >> c03;
+    //vj.agregarComputadora(c03);
 
-    Computadora c04;
-    cin >> c04;
-    vj << c04;    
+    //Computadora c04;
+    //cin >> c04;
+   // vj << c04;    
 
-    vj.mostrar();
-    
+    //vj.mostrar();
+    //vj.respaldar_tabla();
+    //vj.respaldar(); //linea por linea
 
 
     //cout << c02;
