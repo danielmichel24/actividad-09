759be3b274f655d7016beea574a44b1393f878c2
Daniel Neri <daniel.neri5575@alumnos.udg.mx>
2020-10-20 17:35:59 -0500

    implementamos recuperar y respaldar en el .cpp

--- Laboratorio.cpp
+++ Laboratorio.cpp
@@ -1,4 +1,5 @@
 #include "Laboratorio.h"
+#include <fstream>
 
 Laboratorio::Laboratorio()
 {
@@ -38,4 +39,70 @@ void Laboratorio::mostrar()
     }
 }
 
+void Laboratorio::respaldar_tabla()
+{
+    ofstream archivo("Computadoras_tabla.txt");
+    if (archivo.is_open()){
+        archivo << left;
+        archivo << setw(10) << "S0";
+        archivo << setw(10) << "Marca";
+        archivo << setw(10) << "Ram";
+        archivo << setw(10) << "Rom";
+        archivo << endl;
+        for(size_t i = 0; i < cont; i++){
+        Computadora &c = arreglo[i];
+        archivo << c;
+        }
+    }
+    archivo.close();
+}
 
+void Laboratorio::respaldar()
+{
+    ofstream archivo("Computadoras.txt");
+    if (archivo.is_open()){
+        for(size_t i = 0; i < cont; i++){
+        Computadora &c = arreglo[i];
+        archivo << c.getSo() << endl;
+        archivo << c.getMarca() << endl;
+        archivo << c.getRam() << endl;
+        archivo << c.getRom() << endl;
+        }
+    }
+    archivo.close();
+}
+
+void Laboratorio::recuperar()
+{
+    ifstream archivo("computadoras.txt");
+    if (archivo.is_open()){
+        string temp;
+        int ram;
+        int rom;
+        Computadora c;
+
+        while (true)
+        {
+            getline(archivo, temp); //So
+            if (archivo.eof()) {
+            break;
+            }
+            c.setSo(temp);
+
+            getline(archivo, temp); //Marca
+            c.setMarca(temp);
+
+            getline(archivo, temp); //Ram
+            ram = stoi(temp); // stof=string to float
+            c.setRam(ram);
+
+            getline(archivo, temp);
+            rom = stoi(temp); //string to int
+            c.setRom(rom);
+
+            agregarComputadora(c);
+        }
+
+    }
+    archivo.close();
+}
